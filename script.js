966c2c5c8923f17ca3acc1562700662aadcb1fd6
zohreh moazezi <zohre.m548@gmail.com>
2023-01-09 14:29:02 +0330

    time

---
+++ script.js
@@ -0,0 +1,13 @@
+// Difficulty: 2
+
+function clock() {
+    const clockNode = document.querySelector("#clock");
+    return setInterval(() => {
+      let date = new Date();
+      let tick = date.toLocaleTimeString();
+      clockNode.textContent = tick;
+    }, 1000);
+  }
+  
+  clock();
+  
